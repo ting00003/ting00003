3e9523f97992e0f53bf40bf1cdc21e9f8d739759
ting00003 <tingfangxa@gmail.com>
2025-05-16 11:50:04 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,98 @@
+﻿const generatedBibEntries = {
+    "jannach2021survey": {
+        "author": "Jannach, Dietmar and Manzoor, Ahtsham and Cai, Wanling and Chen, Li",
+        "journal": "ACM Computing Surveys (CSUR)",
+        "number": "5",
+        "pages": "1--36",
+        "publisher": "ACM New York, NY, USA",
+        "title": "A survey on conversational recommender systems",
+        "type": "article Dialogue and Conversational Models % Category: 1. Traditional Matching Techniques",
+        "volume": "54",
+        "year": "2021"
+    },
+    "nader2022folk": {
+        "author": "Nader, Karim and Lee, Min Kyung",
+        "booktitle": "International Conference on Information",
+        "organization": "Springer",
+        "pages": "445--458",
+        "title": "Folk theories and user strategies on dating apps: how users understand and manage their experience with algorithmic matchmaking",
+        "type": "inproceedings @inproceedings{nader2022folk, Critical Perspectives and Transparency % Category: 4. Critical Perspectives and Transparency",
+        "year": "2022"
+    },
+    "nayak2010social": {
+        "author": "Nayak, Richi and Zhang, Meng and Chen, Lin",
+        "booktitle": "2010 IEEE international conference on data mining workshops",
+        "organization": "IEEE",
+        "pages": "352--357",
+        "title": "A social matching system for an online dating network: a preliminary study",
+        "type": "inproceedings @inproceedings{nayak2010social, Traditional Matching Techniques % Category: 4. Critical Perspectives and Transparency",
+        "year": "2010"
+    },
+    "pettersen2023construction": {
+        "author": "Pettersen, Lene and D{\\o}ving, Runar",
+        "journal": "Nordic Journal of Science and Technology Studies",
+        "number": "1",
+        "title": "The construction of matches in dating platforms",
+        "type": "article @article{pettersen2023construction, Critical Perspectives and Transparency % Category: 4. Critical Perspectives and Transparency",
+        "volume": "11",
+        "year": "2023"
+    },
+    "pidoux2023comparative": {
+        "author": "Pidoux, Jessica",
+        "journal": "Porn Studies",
+        "number": "2",
+        "pages": "191--209",
+        "publisher": "Taylor \\& Francis",
+        "title": "A comparative study of algorithmic--user classification practices in online dating: a human--machine learning process",
+        "type": "article @article{pidoux2023comparative, Critical Perspectives and Transparency % Category: 4. Critical Perspectives and Transparency",
+        "volume": "10",
+        "year": "2023"
+    },
+    "sharabi2022finding": {
+        "author": "Sharabi, Liesel L",
+        "journal": "Harvard Data Science Review",
+        "number": "1",
+        "pages": "1--11",
+        "publisher": "The MIT Press",
+        "title": "Finding love on a first data: Matching algorithms in online dating",
+        "type": "article @article{sharabi2022finding, Critical Perspectives and Transparency % Category: 2. User Behaviour and Interaction",
+        "volume": "4",
+        "year": "2022"
+    },
+    "soemardi2023testing": {
+        "author": "Soemardi, Calvin Hartono and Radityaputra, Mario Indra and Rahman, Raiza and Gunawan, Alexander Agung Santoso and Saputra, Karen Etania",
+        "booktitle": "2023 8th International Conference on Information Technology and Digital Applications (ICITDA)",
+        "organization": "IEEE",
+        "pages": "1--6",
+        "title": "Testing How Dating Apps Recommend a Potential Matches for User",
+        "type": "inproceedings @inproceedings{soemardi2023testing, User Behaviour and Interaction % Category: 1. Traditional Matching Techniques",
+        "year": "2023"
+    },
+    "thaiprayoon2023enhancing": {
+        "author": "Thaiprayoon, Santipong and Unger, Herwig",
+        "booktitle": "Proceedings of the 2023 7th International Conference on Natural Language Processing and Information Retrieval",
+        "pages": "319--327",
+        "title": "Enhancing a User Matchmaking Algorithm using Personalized PageRank",
+        "type": "inproceedings @inproceedings{thaiprayoon2023enhancing, Traditional Matching Techniques % Category: 2. User Behaviour and Interaction",
+        "year": "2023"
+    },
+    "tomita2022matching": {
+        "author": "Tomita, Yoji and Togashi, Riku and Moriwaki, Daisuke",
+        "booktitle": "Proceedings of the 16th ACM Conference on Recommender Systems",
+        "pages": "538--541",
+        "title": "Matching theory-based recommender systems in online dating",
+        "type": "inproceedings @inproceedings{tomita2022matching, Traditional Matching Techniques",
+        "year": "2022"
+    },
+    "zheng2022you": {
+        "author": "Zheng, Xuanzhi and Zhao, Guoshuai and Zhu, Li and Zhu, Jihua and Qian, Xueming",
+        "journal": "IEEE Transactions on Knowledge and Data Engineering",
+        "number": "5",
+        "pages": "5400--5412",
+        "publisher": "IEEE",
+        "title": "What you like, what i am: online dating recommendation via matching individual preferences with features",
+        "type": "article @article{zheng2022you, User Behaviour and Interaction",
+        "volume": "35",
+        "year": "2022"
+    }
+};
